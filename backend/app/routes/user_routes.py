717618a86004e1c03698420050acd30958571455
KauaHenSilva <kaua.sbc@gmail.com>
2025-03-26 11:30:47 -0300

    refactor: remove example user route

--- backend/app/routes/user_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-bp = Blueprint('example', __name__)
-
-@bp.route('/example', methods=['GET'])
-def example():
-    return jsonify({"message": "Hello, PostgreSQL!"})
