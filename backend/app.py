8d306373a8b257160022b24a9668d645cffb804a
KauaHenSilva <kaua.sbc@gmail.com>
2025-03-26 10:19:26 -0300

    Adiciona funcionalidade de recuperação de usuário por ID e estrutura de banco de dados com tabelas para usuários, transações e cartões

--- backend/app.py
+++ backend/app.py
@@ -29,6 +29,10 @@ class Banco:
         self.cursor.execute(f"INSERT INTO usuarios (nome, sobrenome, email, senha) VALUES ('{nome}', '{sobrenome}', '{email}', '{senha}')")
         self.connection.commit()
     
+    def get_user_por_id(self, id):
+        self.cursor.execute(f"SELECT * FROM users WHERE idUser = {id}")
+        return self.cursor.fetchone()
+    
 app = Flask(__name__)
 banco = Banco()
 
@@ -42,6 +46,11 @@ def cadastro():
     print(dados)
     banco.cadastrar(dados['nome'], dados['sobrenome'], dados['email'], dados['senha'])
     return jsonify({"mensagem": "Cadastro realizado com sucesso!", "dados": dados})
+
+@app.route('/api/usuario/<int:id>', methods=['GET'])
+def get_cartao(id):
+    usuario = banco.get_user_por_id(id)
+    return jsonify(usuario)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
